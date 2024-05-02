fd9eeeadb26767003a78dd638050735afaf6f912
aKai <fahim.ak24@gmail.com>
2024-05-02 13:55:34 +0600

    project start and basic layout

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+
+    <div class="py-8 space-y-4">
+        <h3 class="font-bold">Video Producer</h3>
+        <p class="text-sm">Full Time - From $60,000</p>
+    </div>
+
+    <div class="flex justify-between items-end mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+    </div>
+</div>
